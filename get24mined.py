eb61a298f715070cf456faaf4460fd0844dd55ca
ggs134 <inditow@gmail.com>
2016-08-27 11:02:07 +0900

    profit changed

--- get24mined.py
+++ get24mined.py
@@ -2,49 +2,52 @@ import requests
 import datetime
 
 def getMinedEther():
-    res = requests.get("http://ethereum.miningpoolhub.com/index.php?page=api&action=getusertransactions&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
-    data = res.json()['getusertransactions']['data']['transactions']
-    credits = []
-    fee = []
-
-    for i in range(len(data)):
-        transaction = data[i]
-        trTime = datetime.datetime.strptime(transaction["timestamp"], "%Y-%m-%d %H:%M:%S")
-        if transaction["type"] == "Fee":
-            fee.append(data[i]['amount'])
-        elif transaction["type"] == "Credit":
-            credits.append(data[i]['amount'])
-        elif datetime.datetime.now() - trTime > datetime.timedelta(hours=-2):
-            # print "hour2"
-            # print trTime
-            break
-
-    netValue = sum(credits) - sum(fee)
+    # res = requests.get("http://ethereum.miningpoolhub.com/index.php?page=api&action=getusertransactions&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
+    # data = res.json()['getusertransactions']['data']['transactions']
+    # credits = []
+    # fee = []
+    #
+    # for i in range(len(data)):
+    #     transaction = data[i]
+    #     trTime = datetime.datetime.strptime(transaction["timestamp"], "%Y-%m-%d %H:%M:%S")
+    #     if transaction["type"] == "Fee":
+    #         fee.append(data[i]['amount'])
+    #     elif transaction["type"] == "Credit":
+    #         credits.append(data[i]['amount'])
+    #     elif datetime.datetime.now() - trTime > datetime.timedelta(hours=-2):
+    #         # print "hour2"
+    #         # print trTime
+    #         break
+    res = requests.get("http://ethereum.miningpoolhub.com/index.php?page=api&action=getdashboarddata&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
+    netValue = res.json()["getdashboarddata"]["data"]["recent_credits_24hours"]["amount"]
+    # netValue = sum(credits) - sum(fee)
     return netValue
 
 def getMinedEtc():
-    res = requests.get("http://ethereum-classic.miningpoolhub.com/index.php?page=api&action=getusertransactions&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
-    data = res.json()['getusertransactions']['data']['transactions']
-    credits = []
-    fee = []
-
-    for i in range(len(data)):
-        transaction = data[i]
-        trTime = datetime.datetime.strptime(transaction["timestamp"], "%Y-%m-%d %H:%M:%S")
-        # print trTime
-        # print "between : "+repr(datetime.datetime.utcnow() - trTime)
-        # print datetime.timedelta(hours=1)
-        # print trTime
-        if transaction["type"] == "Fee":
-            fee.append(data[i]['amount'])
-        elif transaction["type"] == "Credit":
-            credits.append(data[i]['amount'])
-        elif datetime.datetime.now() - trTime > datetime.timedelta(hours=-1):
-            # print "hour2"
-            # print trTime
-            break
-
-    netValue = sum(credits) - sum(fee)
+    # res = requests.get("http://ethereum-classic.miningpoolhub.com/index.php?page=api&action=getusertransactions&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
+    # data = res.json()['getusertransactions']['data']['transactions']
+    # credits = []
+    # fee = []
+    #
+    # for i in range(len(data)):
+    #     transaction = data[i]
+    #     trTime = datetime.datetime.strptime(transaction["timestamp"], "%Y-%m-%d %H:%M:%S")
+    #     # print trTime
+    #     # print "between : "+repr(datetime.datetime.utcnow() - trTime)
+    #     # print datetime.timedelta(hours=1)
+    #     # print trTime
+    #     if transaction["type"] == "Fee":
+    #         fee.append(data[i]['amount'])
+    #     elif transaction["type"] == "Credit":
+    #         credits.append(data[i]['amount'])
+    #     elif datetime.datetime.now() - trTime > datetime.timedelta(hours=-1):
+    #         # print "hour2"
+    #         # print trTime
+    #         break
+    #
+    # netValue = sum(credits) - sum(fee)
+    res = requests.get("http://ethereum-classic.miningpoolhub.com/index.php?page=api&action=getdashboarddata&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
+    netValue = res.json()["getdashboarddata"]["data"]["recent_credits_24hours"]["amount"]
     return netValue
 
 def getWeatherInfo():
