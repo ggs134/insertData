2f4ab70f8ecfcc1142c77fd243dff4d743ff66f8
ggs134 <inditow@gmail.com>
2016-09-02 21:29:00 +0900

    연구실 업데이트

--- insertData.py
+++ insertData.py
@@ -9,13 +9,14 @@ import time
 mongoClient = pymongo.MongoClient("127.0.0.1",27017)
 mongoDB = mongoClient.di
 
-miners_farm1 = [1,2,3,4,5,6,8,14]
-miners_farm2 = [9,10,11,12]
+miners_farm1 = [1,2,3,4,5,6,8]
+miners_farm2 = [9,10,11,12,14]
 miners_farm3 = [i for i in range(15,39) if i is not 37 ]
 miner_list = miners_farm1 + miners_farm2 + miners_farm3
 
 data1 = []
 data2 = []
+data3 = []
 for i in miners_farm1:
   try:
     res = mongoDB["miner"+str(i)].find(sort=[("_id",-1)]).limit(1).next()
@@ -44,17 +45,35 @@ for i in miners_farm3:
     boot_hour = (time.time() - float(res["bootTime"]))/3600
     rs = {"boot_hour":boot_hour, "username": res["username"], "hash":(res["hashrate"]+res["hashrateC"])/1000.0, "temp":temp, "average_temp": averageTemp, "gpu_num":len(temp)}
     # print rs
+    data3.append(rs)
+  except Exception as e:
+    print e
+
+for i in miners_farm2:
+  try:
+    res = mongoDB["miner"+str(i)].find(sort=[("_id",-1)]).limit(1).next()
+    # print res
+    # print res["hashrate"]
+    # rs = {"hash":res["hashrate"]+res["hashrateC"], "temp":res["gpuTemprature"], "fanspeed":res["fanspeed"]}
+    # print res
+    temp = [int(j) for j in res["gpuTemperature"].strip("[]").split(",")]
+    averageTemp = sum(temp) / float(len(temp))
+    boot_hour = (time.time() - float(res["bootTime"]))/3600
+    rs = {"boot_hour":boot_hour, "username": res["username"], "hash":(res["hashrate"]+res["hashrateC"])/1000.0, "temp":temp, "average_temp": averageTemp, "gpu_num":len(temp)}
+    # print rs
     data2.append(rs)
   except Exception as e:
     print e
 
-total_data = data1 + data2
+total_data = data1 + data2 + data3
 average_list1 = [i["average_temp"] for i in data1 ]
 average_list2 = [ i["average_temp"] for i in data2 ]
-total_average_list = average_list1 + average_list2
+average_list3 = [ i["average_temp"] for i in data3 ]
+total_average_list = average_list1 + average_list2 + average_list3
 total_average = sum(total_average_list) / float(len(total_average_list))
 average1 = sum(average_list1) / float(len(average_list1))
 average2 = sum(average_list2) / float(len(average_list2))
+average3 = sum(average_list2) / float(len(average_list2))
 
 max_temp = max(max([i["temp"] for i in total_data]))
 max_list = [i["username"] for i in total_data if max_temp in i["temp"]]
@@ -81,3 +100,4 @@ mongoDB["profit"].insert(profit)
 mongoDB["weather"].insert(weather)
 mongoDB["statusData1"].insert({"data": data1})
 mongoDB["statusData2"].insert({"data": data2})
+mongoDB["statusData3"].insert({"data": data3})
